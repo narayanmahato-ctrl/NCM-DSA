1d4b0cfb32f63108d55881eb8d6b5ee48962c450
Narayan <narayanmahato159@gmail.com>
2025-07-08 21:48:44 +0530

    Queue in LinkedList using java Framework

---
+++ Frame.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Frame {
+    public static void main(String[] args){
+        Queue<Integer> q=new LinkedList<>();
+        q.add(34);
+        q.add(56);
+        System.out.println(q);
+    }
+    
+}
