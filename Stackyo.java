bb2ec768f876fd85d37d40c75d2404157c97b960
Narayan <narayanmahato159@gmail.com>
2025-07-08 21:53:05 +0530

    Stack using java framework

---
+++ Stackyo.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Stackyo {
+    public static void main(String[] args){
+        Stack <Integer> s=new Stack<>();
+        s.add(567);
+        s.add(34);
+        System.out.println(s);
+    }
+    
+}
