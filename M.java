6b2e49cc358b1099681d990ab5cfce84ee2c9960
Narayan <narayanmahato159@gmail.com>
2025-07-22 23:24:36 +0530

    print maze with jump

--- M.java
+++ M.java
@@ -3,7 +3,7 @@ import java.util.*;
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class M {
 
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
